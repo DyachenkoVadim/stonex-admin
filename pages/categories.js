9e26c802138b42688de45f5dd19f88baacb53b25
Vadim <vadimdyachenko000@gmail.com>
2025-01-20 20:38:04 +0200

    add new category func

---
+++ pages/categories.js
@@ -0,0 +1,32 @@
+import Layout from "@/components/Layout";
+import axios from "axios";
+import { useState } from "react";
+
+export default function Categories() {
+  const [name, setName] = useState("");
+
+  const saveCategory = async (ev) => {
+    ev.preventDefault();
+    await axios.post("/api/categories", { name });
+    setName("");
+  };
+
+  return (
+    <Layout>
+      <h1>Categories</h1>
+      <label>New category name</label>
+      <form onSubmit={saveCategory} className="flex gap-1">
+        <input
+          className="mb-0"
+          type="text"
+          placeholder="Category name"
+          value={name}
+          onChange={(ev) => setName(ev.target.value)}
+        />
+        <button type="submit" className="btn-primary py-1">
+          Save
+        </button>
+      </form>
+    </Layout>
+  );
+}
