cfbe1bc87084386f5f085da785b8eaca397774e6
Vadim <vadimdyachenko000@gmail.com>
2025-01-23 17:29:19 +0200

    edit and delete category func

--- pages/categories.js
+++ pages/categories.js
@@ -2,11 +2,13 @@ import Layout from "@/components/Layout";
 import { Category } from "@/models/Category";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { withSwal } from "react-sweetalert2";
 
-export default function Categories() {
+function Categories({ swal }) {
   const [name, setName] = useState("");
   const [categories, setCategories] = useState([]);
   const [parentCategory, setParentCategory] = useState("");
+  const [editedCategory, setEditedCategory] = useState(null);
 
   useEffect(() => {
     fetchCategories();
@@ -20,15 +22,56 @@ export default function Categories() {
 
   const saveCategory = async (ev) => {
     ev.preventDefault();
-    await axios.post("/api/categories", { name, parentCategory });
+    const data = {
+      name,
+      parentCategory,
+    };
+    if (editedCategory) {
+      data._id = editedCategory._id;
+      await axios.put("/api/categories", data);
+      setEditedCategory(null);
+    } else {
+      await axios.post("/api/categories", data);
+    }
     setName("");
+    setParentCategory("");
     fetchCategories();
+  };
+
+  const editCategory = (category) => {
+    setEditedCategory(category);
+    setName(category.name);
+    setParentCategory(category.parent?._id);
+  };
+
+  const deleteCategory = (category) => {
+    swal
+      .fire({
+        title: "Are you sure?",
+        text: `Do you want to delete ${category.name}?`,
+        showCancelButton: true,
+        cancelButtonText: "Cancel",
+        confirmButtonText: "Yes, Delete!",
+        confirmButtonColor: "#d55",
+        reverseButtons: true,
+      })
+      .then(async (result) => {
+        if (result.isConfirmed) {
+          const { _id } = category;
+          await axios.delete("/api/categories?_id=" + _id);
+          fetchCategories();
+        }
+      });
   };
 
   return (
     <Layout>
       <h1>Categories</h1>
-      <label>New category name</label>
+      <label>
+        {editedCategory
+          ? `Edit category ${editedCategory.name}`
+          : "New category name"}
+      </label>
       <form onSubmit={saveCategory} className="flex gap-1">
         <input
           className="mb-0"
@@ -41,7 +84,7 @@ export default function Categories() {
           value={parentCategory}
           onChange={(ev) => setParentCategory(ev.target.value)}
         >
-          <option>No parent category</option>
+          <option value={undefined}>No parent category</option>
           {categories.length > 0 &&
             categories.map((category) => (
               <option value={category._id}>{category.name}</option>
@@ -56,6 +99,7 @@ export default function Categories() {
           <tr>
             <td>Category name</td>
             <td>Parent category</td>
+            <td></td>
           </tr>
         </thead>
         <tbody>
@@ -64,6 +108,49 @@ export default function Categories() {
               <tr>
                 <td>{category.name}</td>
                 <td>{category?.parent?.name}</td>
+                <td>
+                  <button
+                    className="btn-primary mr-1 inline-flex items-center"
+                    onClick={() => editCategory(category)}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="size-4"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125"
+                      />
+                    </svg>
+                    Edit
+                  </button>
+                  <button
+                    className="btn-primary inline-flex items-center"
+                    onClick={() => deleteCategory(category)}
+                  >
+                    {" "}
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="size-4"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                      />
+                    </svg>
+                    Delete
+                  </button>
+                </td>
               </tr>
             ))}
         </tbody>
@@ -71,5 +158,7 @@ export default function Categories() {
     </Layout>
   );
 }
+
+export default withSwal(({ swal }, ref) => <Categories swal={swal} />);
 
 //3:32:48
